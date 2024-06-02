5629ce740b1ed8d4f97bddc5d50a6ebe2723014e
Sultan Firdaus <sultanfirdaus324@gmail.com>
2024-06-03 01:41:53 +0700

    third commit

--- php-fpm/vendor/composer/installed.php
+++ php-fpm/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '051716d6d0fab65944b39e32c23ddb8c3307e59e',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '051716d6d0fab65944b39e32c23ddb8c3307e59e',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
